7a6b098747366937dfb77e54d80bdf17e8b176a0
Przemek <przemek@przemyk.xyz>
2020-07-09 21:40:57 +0200

    Biomes O' Plenty support without recipes

--- src/main/java/xyz/przemyk/simpleplanes/setup/SimplePlanesItems.java
+++ src/main/java/xyz/przemyk/simpleplanes/setup/SimplePlanesItems.java
@@ -64,4 +64,37 @@ public class SimplePlanesItems {
     public static final RegistryObject<PlaneItem> FT_CITRUS_PLANE = ITEMS.register("ft_citrus_plane", () -> new PlaneItem(new Item.Properties().group(SIMPLE_PLANES_ITEM_GROUP), world -> new PlaneEntity(SimplePlanesEntities.FT_CITRUS_PLANE.get(), world)));
     public static final RegistryObject<PlaneItem> FR_CITRUS_LARGE_PLANE = ITEMS.register("ft_citrus_large_plane", () -> new PlaneItem(new Item.Properties().group(SIMPLE_PLANES_ITEM_GROUP), world -> new LargePlaneEntity(SimplePlanesEntities.FR_CITRUS_LARGE_PLANE.get(), world)));
 
+    //////////// Biomes O' Plenty
+    public static final RegistryObject<PlaneItem> BOP_CHERRY_PLANE = ITEMS.register("bop_cherry_plane", () -> new PlaneItem(new Item.Properties().group(SIMPLE_PLANES_ITEM_GROUP), world -> new PlaneEntity(SimplePlanesEntities.BOP_CHERRY_PLANE.get(), world)));
+    public static final RegistryObject<PlaneItem> BOP_CHERRY_LARGE_PLANE = ITEMS.register("bop_cherry_large_plane", () -> new PlaneItem(new Item.Properties().group(SIMPLE_PLANES_ITEM_GROUP), world -> new LargePlaneEntity(SimplePlanesEntities.BOP_CHERRY_LARGE_PLANE.get(), world)));
+
+    public static final RegistryObject<PlaneItem> BOP_DEAD_PLANE = ITEMS.register("bop_dead_plane", () -> new PlaneItem(new Item.Properties().group(SIMPLE_PLANES_ITEM_GROUP), world -> new PlaneEntity(SimplePlanesEntities.BOP_DEAD_PLANE.get(), world)));
+    public static final RegistryObject<PlaneItem> BOP_DEAD_LARGE_PLANE = ITEMS.register("bop_dead_large_plane", () -> new PlaneItem(new Item.Properties().group(SIMPLE_PLANES_ITEM_GROUP), world -> new LargePlaneEntity(SimplePlanesEntities.BOP_DEAD_LARGE_PLANE.get(), world)));
+
+    public static final RegistryObject<PlaneItem> BOP_FIR_PLANE = ITEMS.register("bop_fir_plane", () -> new PlaneItem(new Item.Properties().group(SIMPLE_PLANES_ITEM_GROUP), world -> new PlaneEntity(SimplePlanesEntities.BOP_FIR_PLANE.get(), world)));
+    public static final RegistryObject<PlaneItem> BOP_FIR_LARGE_PLANE = ITEMS.register("bop_fir_large_plane", () -> new PlaneItem(new Item.Properties().group(SIMPLE_PLANES_ITEM_GROUP), world -> new LargePlaneEntity(SimplePlanesEntities.BOP_FIR_LARGE_PLANE.get(), world)));
+
+    public static final RegistryObject<PlaneItem> BOP_HELLBARK_PLANE = ITEMS.register("bop_hellbark_plane", () -> new PlaneItem(new Item.Properties().group(SIMPLE_PLANES_ITEM_GROUP), world -> new PlaneEntity(SimplePlanesEntities.BOP_HELLBARK_PLANE.get(), world)));
+    public static final RegistryObject<PlaneItem> BOP_HELLBARK_LARGE_PLANE = ITEMS.register("bop_hellbark_large_plane", () -> new PlaneItem(new Item.Properties().group(SIMPLE_PLANES_ITEM_GROUP), world -> new LargePlaneEntity(SimplePlanesEntities.BOP_HELLBARK_LARGE_PLANE.get(), world)));
+
+    public static final RegistryObject<PlaneItem> BOP_JACARANDA_PLANE = ITEMS.register("bop_jacaranda_plane", () -> new PlaneItem(new Item.Properties().group(SIMPLE_PLANES_ITEM_GROUP), world -> new PlaneEntity(SimplePlanesEntities.BOP_JACARANDA_PLANE.get(), world)));
+    public static final RegistryObject<PlaneItem> BOP_JACARANDA_LARGE_PLANE = ITEMS.register("bop_jacaranda_large_plane", () -> new PlaneItem(new Item.Properties().group(SIMPLE_PLANES_ITEM_GROUP), world -> new LargePlaneEntity(SimplePlanesEntities.BOP_JACARANDA_LARGE_PLANE.get(), world)));
+
+    public static final RegistryObject<PlaneItem> BOP_MAGIC_PLANE = ITEMS.register("bop_magic_plane", () -> new PlaneItem(new Item.Properties().group(SIMPLE_PLANES_ITEM_GROUP), world -> new PlaneEntity(SimplePlanesEntities.BOP_MAGIC_PLANE.get(), world)));
+    public static final RegistryObject<PlaneItem> BOP_MAGIC_LARGE_PLANE = ITEMS.register("bop_magic_large_plane", () -> new PlaneItem(new Item.Properties().group(SIMPLE_PLANES_ITEM_GROUP), world -> new LargePlaneEntity(SimplePlanesEntities.BOP_MAGIC_LARGE_PLANE.get(), world)));
+
+    public static final RegistryObject<PlaneItem> BOP_MAHOGANY_PLANE = ITEMS.register("bop_mahogany_plane", () -> new PlaneItem(new Item.Properties().group(SIMPLE_PLANES_ITEM_GROUP), world -> new PlaneEntity(SimplePlanesEntities.BOP_MAHOGANY_PLANE.get(), world)));
+    public static final RegistryObject<PlaneItem> BOP_MAHOGANY_LARGE_PLANE = ITEMS.register("bop_mahogany_large_plane", () -> new PlaneItem(new Item.Properties().group(SIMPLE_PLANES_ITEM_GROUP), world -> new LargePlaneEntity(SimplePlanesEntities.BOP_MAHOGANY_LARGE_PLANE.get(), world)));
+
+    public static final RegistryObject<PlaneItem> BOP_PALM_PLANE = ITEMS.register("bop_palm_plane", () -> new PlaneItem(new Item.Properties().group(SIMPLE_PLANES_ITEM_GROUP), world -> new PlaneEntity(SimplePlanesEntities.BOP_PALM_PLANE.get(), world)));
+    public static final RegistryObject<PlaneItem> BOP_PALM_LARGE_PLANE = ITEMS.register("bop_palm_large_plane", () -> new PlaneItem(new Item.Properties().group(SIMPLE_PLANES_ITEM_GROUP), world -> new LargePlaneEntity(SimplePlanesEntities.BOP_PALM_LARGE_PLANE.get(), world)));
+
+    public static final RegistryObject<PlaneItem> BOP_REDWOOD_PLANE = ITEMS.register("bop_redwood_plane", () -> new PlaneItem(new Item.Properties().group(SIMPLE_PLANES_ITEM_GROUP), world -> new PlaneEntity(SimplePlanesEntities.BOP_REDWOOD_PLANE.get(), world)));
+    public static final RegistryObject<PlaneItem> BOP_REDWOOD_LARGE_PLANE = ITEMS.register("bop_redwood_large_plane", () -> new PlaneItem(new Item.Properties().group(SIMPLE_PLANES_ITEM_GROUP), world -> new LargePlaneEntity(SimplePlanesEntities.BOP_REDWOOD_LARGE_PLANE.get(), world)));
+
+    public static final RegistryObject<PlaneItem> BOP_UMBRAN_PLANE = ITEMS.register("bop_umbran_plane", () -> new PlaneItem(new Item.Properties().group(SIMPLE_PLANES_ITEM_GROUP), world -> new PlaneEntity(SimplePlanesEntities.BOP_UMBRAN_PLANE.get(), world)));
+    public static final RegistryObject<PlaneItem> BOP_UMBRAN_LARGE_PLANE = ITEMS.register("bop_umbran_large_plane", () -> new PlaneItem(new Item.Properties().group(SIMPLE_PLANES_ITEM_GROUP), world -> new LargePlaneEntity(SimplePlanesEntities.BOP_UMBRAN_LARGE_PLANE.get(), world)));
+
+    public static final RegistryObject<PlaneItem> BOP_WILLOW_PLANE = ITEMS.register("bop_willow_plane", () -> new PlaneItem(new Item.Properties().group(SIMPLE_PLANES_ITEM_GROUP), world -> new PlaneEntity(SimplePlanesEntities.BOP_WILLOW_PLANE.get(), world)));
+    public static final RegistryObject<PlaneItem> BOP_WILLOW_LARGE_PLANE = ITEMS.register("bop_willow_large_plane", () -> new PlaneItem(new Item.Properties().group(SIMPLE_PLANES_ITEM_GROUP), world -> new LargePlaneEntity(SimplePlanesEntities.BOP_WILLOW_LARGE_PLANE.get(), world)));
 }
