e571ac7ea3f7a090d778ecdc71635583deb63cca
Przemyk <przemek@przemyk.xyz>
2022-04-05 19:51:12 +0200

    entity_nbt payloads

--- src/main/java/xyz/przemyk/simpleplanes/datapack/PlanePayloadReloadListener.java
+++ src/main/java/xyz/przemyk/simpleplanes/datapack/PlanePayloadReloadListener.java
@@ -4,6 +4,8 @@ import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.nbt.TagParser;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.server.packs.resources.ResourceManager;
 import net.minecraft.server.packs.resources.SimpleJsonResourceReloadListener;
@@ -38,8 +40,16 @@ public class PlanePayloadReloadListener extends SimpleJsonResourceReloadListener
                 Item item = Objects.requireNonNull(ForgeRegistries.ITEMS.getValue(new ResourceLocation(jsonObject.get("item").getAsString())), "missing item");
                 Block renderBlock = Objects.requireNonNull(ForgeRegistries.BLOCKS.getValue(new ResourceLocation((jsonObject.get("block").getAsString()))), "missing block");
                 EntityType<?> dropSpawnEntity = Objects.requireNonNull(ForgeRegistries.ENTITIES.getValue(new ResourceLocation(jsonObject.get("entity").getAsString())), "missing entity");
+                CompoundTag compoundTag;
+                if (jsonObject.has("entity_nbt")) {
+                    String tag = GsonHelper.convertToString(jsonObject.get("entity_nbt"), "entity_nbt");
+                    System.out.println(tag);
+                    compoundTag = TagParser.parseTag(tag);
+                } else {
+                    compoundTag = new CompoundTag();
+                }
 
-                payloadEntries.put(item, new PayloadEntry(item, renderBlock, dropSpawnEntity));
+                payloadEntries.put(item, new PayloadEntry(item, renderBlock, dropSpawnEntity, compoundTag));
             } catch (Exception e) {
                 LOGGER.error("Parsing error loading plane payload {}", entry.getKey(), e);
             }
