19453edef5b00a629f2826a5998caecaba3577c2
Przemyk <przemek@przemyk.xyz>
2022-09-15 17:29:58 +0200

    mr crayfish gun mod support

--- src/main/java/xyz/przemyk/simpleplanes/upgrades/shooter/ShooterUpgrade.java
+++ src/main/java/xyz/przemyk/simpleplanes/upgrades/shooter/ShooterUpgrade.java
@@ -1,24 +1,42 @@
 package xyz.przemyk.simpleplanes.upgrades.shooter;
 
+import com.mojang.blaze3d.vertex.PoseStack;
 import com.mojang.math.Vector3f;
 import net.minecraft.Util;
+import net.minecraft.core.Direction;
+import net.minecraft.nbt.CompoundTag;
 import net.minecraft.network.FriendlyByteBuf;
 import net.minecraft.util.RandomSource;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.entity.projectile.*;
+import net.minecraft.world.inventory.DataSlot;
+import net.minecraft.world.inventory.Slot;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Items;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.phys.Vec3;
+import net.minecraftforge.common.capabilities.Capability;
+import net.minecraftforge.common.util.LazyOptional;
 import net.minecraftforge.event.entity.player.PlayerInteractEvent;
+import net.minecraftforge.fml.ModList;
+import net.minecraftforge.items.CapabilityItemHandler;
+import net.minecraftforge.items.ItemStackHandler;
+import net.minecraftforge.items.SlotItemHandler;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+import xyz.przemyk.simpleplanes.client.gui.PlaneInventoryScreen;
+import xyz.przemyk.simpleplanes.compat.MrCrayfishGunCompat;
 import xyz.przemyk.simpleplanes.entities.PlaneEntity;
 import xyz.przemyk.simpleplanes.setup.SimplePlanesUpgrades;
 import xyz.przemyk.simpleplanes.upgrades.Upgrade;
 
+import java.util.function.Function;
+
 public class ShooterUpgrade extends Upgrade {
 
-    private boolean shootSide = false;
+    public final ItemStackHandler itemStackHandler = new ItemStackHandler();
+    public final LazyOptional<ItemStackHandler> itemStackHandlerLazyOptional = LazyOptional.of(() -> itemStackHandler);
 
     public ShooterUpgrade(PlaneEntity planeEntity) {
         super(SimplePlanesUpgrades.SHOOTER.get(), planeEntity);
@@ -27,84 +45,122 @@ public class ShooterUpgrade extends Upgrade {
     @Override
     public void onItemRightClick(PlayerInteractEvent.RightClickItem event) {
         Player player = event.getEntity();
-        ItemStack itemStack = player.getItemInHand(event.getHand());
         Vector3f motion1 = planeEntity.transformPos(new Vector3f(0, 0, (float) (1 + planeEntity.getDeltaMovement().length())));
         Vec3 motion = new Vec3(motion1);
-        Level world = event.getLevel();
-        RandomSource random = world.random;
+        Level level = event.getLevel();
+        RandomSource random = level.random;
 
-        Vector3f pos = planeEntity.transformPos(new Vector3f(shootSide ? 0.8f : -0.8f, 0.8f, 0.8f));
-        shootSide = !shootSide;
+        Vector3f pos = planeEntity.transformPos(new Vector3f(0.0f, 0.8f, 0.8f));
         updateClient();
 
         double x = pos.x() + planeEntity.getX();
         double y = pos.y() + planeEntity.getY();
         double z = pos.z() + planeEntity.getZ();
 
+        ItemStack itemStack = itemStackHandler.getStackInSlot(0);
         Item item = itemStack.getItem();
 
         if (item == Items.FIREWORK_ROCKET) {
-            FireworkRocketEntity fireworkrocketentity = new FireworkRocketEntity(world, itemStack, x, y, z, true);
+            FireworkRocketEntity fireworkrocketentity = new FireworkRocketEntity(level, itemStack, x, y, z, true);
             fireworkrocketentity.shoot(-motion.x, -motion.y, -motion.z, -(float) Math.max(0.5F, motion.length() * 1.5), 1.0F);
-            world.addFreshEntity(fireworkrocketentity);
+            level.addFreshEntity(fireworkrocketentity);
             if (!player.isCreative()) {
-                itemStack.shrink(1);
+                itemStackHandler.extractItem(0, 1, false);
             }
         } else if (item == Items.FIRE_CHARGE) {
             double d3 = random.nextGaussian() * 0.05D + 2 * motion.x;
             double d4 = random.nextGaussian() * 0.05D;
             double d5 = random.nextGaussian() * 0.05D + 2 * motion.z;
             Fireball fireBallEntity = Util
-                .make(new SmallFireball(world, player, d3, d4, d5), (p_229425_1_) -> p_229425_1_.setItem(itemStack));
+                .make(new SmallFireball(level, player, d3, d4, d5), (p_229425_1_) -> p_229425_1_.setItem(itemStack));
             fireBallEntity.setPos(x, y, z);
             fireBallEntity.setDeltaMovement(motion.scale(2));
-            world.addFreshEntity(fireBallEntity);
+            level.addFreshEntity(fireBallEntity);
             if (!player.isCreative()) {
-                itemStack.shrink(1);
+                itemStackHandler.extractItem(0, 1, false);
             }
         } else if (item == Items.ARROW) {
-            Arrow arrowEntity = new Arrow(world, x, y, z);
+            Arrow arrowEntity = new Arrow(level, x, y, z);
             arrowEntity.setOwner(player);
             arrowEntity.setDeltaMovement(motion.scale(Math.max(motion.length() * 1.5, 3) / motion.length()));
             if (!player.isCreative()) {
-                itemStack.shrink(1);
+                itemStackHandler.extractItem(0, 1, false);
                 arrowEntity.pickup = AbstractArrow.Pickup.ALLOWED;
             }
-            world.addFreshEntity(arrowEntity);
+            level.addFreshEntity(arrowEntity);
         } else if (item == Items.TIPPED_ARROW) {
-            Arrow arrowEntity = new Arrow(world, x, y, z);
+            Arrow arrowEntity = new Arrow(level, x, y, z);
             arrowEntity.setOwner(player);
             arrowEntity.setEffectsFromItem(itemStack);
             arrowEntity.setDeltaMovement(motion.scale(Math.max(motion.length() * 1.5, 3) / motion.length()));
             if (!player.isCreative()) {
-                itemStack.shrink(1);
+                itemStackHandler.extractItem(0, 1, false);
                 arrowEntity.pickup = AbstractArrow.Pickup.ALLOWED;
             }
-            world.addFreshEntity(arrowEntity);
+            level.addFreshEntity(arrowEntity);
         } else if (item == Items.SPECTRAL_ARROW) {
-            SpectralArrow arrowEntity = new SpectralArrow(world, x, y, z);
+            SpectralArrow arrowEntity = new SpectralArrow(level, x, y, z);
             arrowEntity.setOwner(player);
             arrowEntity.setDeltaMovement(motion.scale(Math.max(motion.length() * 1.5, 3) / motion.length()));
             if (!player.isCreative()) {
-                itemStack.shrink(1);
+                itemStackHandler.extractItem(0, 1, false);
                 arrowEntity.pickup = AbstractArrow.Pickup.ALLOWED;
             }
-            world.addFreshEntity(arrowEntity);
+            level.addFreshEntity(arrowEntity);
+        } else {
+            ModList.get().getModContainerById("cgm").ifPresent(cgm -> MrCrayfishGunCompat.shooterBehaviour(item, itemStackHandler, level, player, motion, x, y, z));
         }
     }
 
     @Override
+    public void invalidateCaps() {
+        super.invalidateCaps();
+        itemStackHandlerLazyOptional.invalidate();
+    }
+
+    @Override
+    public CompoundTag serializeNBT() {
+        CompoundTag compoundTag = new CompoundTag();
+        compoundTag.put("item", itemStackHandler.serializeNBT());
+        return compoundTag;
+    }
+
+    @Override
+    public void deserializeNBT(CompoundTag compoundTag) {
+        itemStackHandler.deserializeNBT(compoundTag.getCompound("item"));
+    }
+
+    @Override
     public void writePacket(FriendlyByteBuf buffer) {
-        buffer.writeBoolean(shootSide);
+        buffer.writeItem(itemStackHandler.getStackInSlot(0));
     }
 
     @Override
     public void readPacket(FriendlyByteBuf buffer) {
-        shootSide = buffer.readBoolean();
+        itemStackHandler.setStackInSlot(0, buffer.readItem());
+    }
+
+    @Override
+    public @NotNull <T> LazyOptional<T> getCapability(@NotNull Capability<T> cap, @Nullable Direction side) {
+        if (cap == CapabilityItemHandler.ITEM_HANDLER_CAPABILITY) {
+            return itemStackHandlerLazyOptional.cast();
+        }
+        return super.getCapability(cap, side);
     }
 
     @Override
     public void onRemoved() {
         planeEntity.spawnAtLocation(Items.DISPENSER);
+        planeEntity.spawnAtLocation(itemStackHandler.getStackInSlot(0));
+    }
+
+    @Override
+    public void addContainerData(Function<Slot, Slot> addSlot, Function<DataSlot, DataSlot> addDataSlot) {
+        addSlot.apply(new SlotItemHandler(itemStackHandler, 0, 134, 62));
+    }
+
+    @Override
+    public void renderScreenBg(PoseStack poseStack, int x, int y, float partialTicks, PlaneInventoryScreen screen) {
+        screen.blit(poseStack, screen.getGuiLeft() + 133, screen.getGuiTop() + 61, 226, 0, 18, 18);
     }
 }
